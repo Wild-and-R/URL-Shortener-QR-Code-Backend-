0d1c52c51a4f7114ebecce952ae8eced369d1e60
Wildan Rahadian <ryan7300@gmail.com>
2026-02-23 14:09:48 +0700

    CORS update

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,13 @@ import authRoutes from './routes/auth.routes';
 import linkRoutes from './routes/link.routes';
 import redirectRoutes from './routes/redirect.routes';
 
+
 const app = express();
+const cors = require('cors');
 app.use(express.json());
+app.use(cors({
+  origin: 'https://linkzip-zeta.vercel.app/'
+}));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/links', linkRoutes);
